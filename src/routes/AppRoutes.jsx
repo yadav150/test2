cf502be2779ed7ac591e801f39c10fd6e4e5327b
Yadav Subba <yadavsubba2003@gmail.com>
2026-08-05 13:32:07 +0530

    Create AppRoutes.jsx

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,22 @@
+import { Routes, Route } from 'react-router-dom'
+import Dashboard from '../modules/dashboard/Dashboard.jsx'
+import StudentList from '../modules/students/StudentList.jsx'
+import StaffList from '../modules/staff/StaffList.jsx'
+import AttendanceGrid from '../modules/attendance/AttendanceGrid.jsx'
+import FeeLedger from '../modules/fees/FeeLedger.jsx'
+import PayrollLedger from '../modules/payroll/PayrollLedger.jsx'
+
+function AppRoutes() {
+  return (
+    <Routes>
+      <Route path="/" element={<Dashboard />} />
+      <Route path="/students" element={<StudentList />} />
+      <Route path="/staff" element={<StaffList />} />
+      <Route path="/attendance" element={<AttendanceGrid />} />
+      <Route path="/fees" element={<FeeLedger />} />
+      <Route path="/payroll" element={<PayrollLedger />} />
+    </Routes>
+  )
+}
+
+export default AppRoutes
