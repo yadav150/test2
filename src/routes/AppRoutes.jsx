73e18369a0ef94b4a5eed907ed5d925fe7ca672c
Yadav Subba <yadavsubba2003@gmail.com>
2026-08-05 13:36:51 +0530

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 import Dashboard from '../modules/dashboard/Dashboard.jsx'
 import StudentList from '../modules/students/StudentList.jsx'
+import StudentProfile from '../modules/students/StudentProfile.jsx'
 import StaffList from '../modules/staff/StaffList.jsx'
 import AttendanceGrid from '../modules/attendance/AttendanceGrid.jsx'
 import FeeLedger from '../modules/fees/FeeLedger.jsx'
@@ -11,6 +12,7 @@ function AppRoutes() {
     <Routes>
       <Route path="/" element={<Dashboard />} />
       <Route path="/students" element={<StudentList />} />
+      <Route path="/students/:studentId" element={<StudentProfile />} />
       <Route path="/staff" element={<StaffList />} />
       <Route path="/attendance" element={<AttendanceGrid />} />
       <Route path="/fees" element={<FeeLedger />} />
