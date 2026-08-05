5ea512136d446bbce5e68cc836989beb3444b95f
Yadav Subba <yadavsubba2003@gmail.com>
2026-08-05 13:41:23 +0530

    Update AppRoutes.jsx

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -3,6 +3,7 @@ import Dashboard from '../modules/dashboard/Dashboard.jsx'
 import StudentList from '../modules/students/StudentList.jsx'
 import StudentProfile from '../modules/students/StudentProfile.jsx'
 import StaffList from '../modules/staff/StaffList.jsx'
+import StaffProfile from '../modules/staff/StaffProfile.jsx'
 import AttendanceGrid from '../modules/attendance/AttendanceGrid.jsx'
 import FeeLedger from '../modules/fees/FeeLedger.jsx'
 import PayrollLedger from '../modules/payroll/PayrollLedger.jsx'
@@ -14,6 +15,7 @@ function AppRoutes() {
       <Route path="/students" element={<StudentList />} />
       <Route path="/students/:studentId" element={<StudentProfile />} />
       <Route path="/staff" element={<StaffList />} />
+      <Route path="/staff/:staffId" element={<StaffProfile />} />
       <Route path="/attendance" element={<AttendanceGrid />} />
       <Route path="/fees" element={<FeeLedger />} />
       <Route path="/payroll" element={<PayrollLedger />} />
